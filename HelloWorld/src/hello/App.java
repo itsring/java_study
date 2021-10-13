52f8ba04e39e42c70f1cbc198175e13dfd9b6b2c
itsring <ki6123@naver.com>
2021-10-13 11:16:20 +0900

    첫번째 커밋

---
+++ HelloWorld/src/hello/App.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 메인 메소드에서 프로그램 시작, 코드블록 {} 안의 내용을 순서대로 실행
+		System.out.println("하이");
+
+	}
+
+}
---
+++ MainMethod/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 메인 메소드에서 프로그램 시작
+		System.out.println("안녕하소");
+		System.out.println("새 자바 프로그램을");
+		System.out.println("실행 합니다.");
+		// 자동 정렬 ctrl shift f
+
+	}
+
+}
